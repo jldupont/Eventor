359603c3dd5fbc5d036fa56e67050aef843dfcbd
jldupont <github@jldupont.com>
2011-03-18 15:48:32 -0400

    initial layout

--- src/eventor.py
+++ src/eventor.py
@@ -1,53 +1,70 @@
 """
-    Multicast bridge over Dbus
-    
+    Eventor
+        
+    14 Feb 2011: added "playing now" dbus signal
+        
+    Created on 2010-10-21
     @author: jldupont
-    @date: March 2011
 """
-APP_NAME="eventor"
-APP_ICON = "eventor"
-ICON_PATH="/usr/share/icons/"
-ICON_FILE="eventor.png"
-LOG_PATH="~/eventor.log"
-HELP_URL="http://www.systemical.com/doc/opensource/eventor"
-TIME_BASE=250  ##milliseconds
-TICKS_SECOND=1000/TIME_BASE
-       
-
-import os
 import sys
 
-## For development environment
-ppkg=os.path.abspath( os.getcwd() +"/eventor")
-if os.path.exists(ppkg):
-    sys.path.insert(0, ppkg)
+APP_NAME="Eventor"
+ICON_NAME="eventor.png"
+HELP_URL="http://www.systemical.com/doc/opensource/eventor"
+TIME_BASE=5000
 
-import gobject
-import dbus.glib
+###<<< DEVELOPMENT MODE SWITCHES
+MSWITCH_OBSERVE_MODE=False
+MSWITCH_DEBUGGING_MODE=False
+MSWITCH_DEBUG_INTEREST=False
+DEV_MODE=True
+###>>>
+
+import gobject, dbus.glib, gtk
 from dbus.mainloop.glib import DBusGMainLoop
-import gtk
 
-gobject.threads_init()
+gobject.threads_init()  #@UndefinedVariable
 dbus.glib.init_threads()
 DBusGMainLoop(set_as_default=True)
 
-from app.system import mswitch
+from eventor.system import base as base
+base.debug=DEV_MODE
+base.debug_interest=MSWITCH_DEBUG_INTEREST
 
-## ------------------------------
-## configurables
-from app.agents.tray import TrayAgent
-_ta=TrayAgent(APP_NAME, ICON_PATH, ICON_FILE)
+from eventor.system import mswitch #@UnusedImport
+mswitch.observe_mode=MSWITCH_OBSERVE_MODE
+mswitch.debugging_mode=MSWITCH_DEBUGGING_MODE
 
-from app.agents.logger import LoggerAgent
-_la=LoggerAgent(APP_NAME, LOG_PATH)
-_la.start()
+from eventor.agents.notifier import notify, NotifierAgent #@Reimport
+from eventor.agents.clock import Clock #@Reimport
 
+def main(debug=False):
+    try:
+        
+        from jld_scripts.res import get_res_path
+        icon_path=get_res_path()
+        
+        from eventor.agents.eventor_tray import TrayAgent
+        _ta=TrayAgent(APP_NAME, icon_path, ICON_NAME, HELP_URL)
 
-from app.agents import adbus
+        import eventor.agents.adbus #@UnusedImport
 
+        
+        _na=NotifierAgent(APP_NAME, ICON_NAME)
+        _na.start()
+        
+        clk=Clock(TIME_BASE)
+        gobject.timeout_add(TIME_BASE, clk.tick)
+        
+        mswitch.publish("__main__", "debug", debug)
+        
+        gtk.main()
+    except KeyboardInterrupt:
+        mswitch.quit()
+        sys.exit(1)        
+        
+    except Exception,e:
+        notify(APP_NAME, "There was an error: %s" % e)
+        mswitch.quit()
+        sys.exit(1)
 
-from app.agents.ui import UiAgent
-ui=UiAgent(HELP_URL, TICKS_SECOND)
-gobject.timeout_add(TIME_BASE, ui.tick)
-
-gtk.main()
